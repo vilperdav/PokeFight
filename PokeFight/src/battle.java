6b6b8035a6f5ad5923ed706e9a0efa98f80cf2e5
vilperdav <vilperdav@gmail.com>
2023-11-21 12:21:31 +0100

    JSON update and learning

--- PokeFight/src/battle.java
+++ PokeFight/src/battle.java
@@ -1,3 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/*
+ * Compilar:    javac -cp lib\json-simple-1.1.1.jar src\*.java
+ *              java -cp lib\json-simple-1.1.1.jar;src battle
+ */
+
 public class battle {
 
+    private ArrayList[] pokemon;
+
+    // MAIN CLASS OF THE GAME
+    public static void main(String[] args) {
+
+        System.out.println("Juego Iniciado - Estoy en MAINs. \n");
+
+        // CHARGE THE POKEMON JSON DB IN THE MAIN CODE
+        String jsonPokemonDB = "db/pokemonDB.json";
+        int numberOfPokemonCharged = pokemonCharger(jsonPokemonDB);
+
+        System.out.println("Se han cargado " + numberOfPokemonCharged + " pokemons. \n");
+
+    }
+
+    // FUNCTION FOR CHARGE THE JSON INTO JAVA OBJECTS TO PLAY WITH THEM
+    public static int pokemonCharger(String jsonPokemonDB) {
+
+        // VARIABLE FOR KNOW WHAT POKEMONS ARE CHARGED IN THE GAME
+        int pokemonCarged = 0;
+
+        try {
+            JSONParser parser = new JSONParser();
+            FileReader reader = new FileReader(jsonPokemonDB);
+            Object obj = parser.parse(reader);
+
+            JSONObject pJsonObj = (JSONObject) obj;
+            JSONArray arrayAllPokemons = (JSONArray) pJsonObj.get("Pokemon");
+
+            for (Object genObj : arrayAllPokemons) {
+                if (genObj instanceof JSONObject) {
+                    JSONObject gen = (JSONObject) genObj;
+
+                    for (Object genKey : gen.keySet()) {
+                        if (genKey instanceof String) {
+                            String genName = (String) genKey;
+                            System.out.println("Generacion: " + genName);
+
+                            JSONObject genDetails = (JSONObject) gen.get(genName);
+
+                            for (Object pokemonKey : genDetails.keySet()) {
+                                if (pokemonKey instanceof String) {
+                                    String pokemonName = (String) pokemonKey;
+                                    JSONObject pokemonDetails = (JSONObject) genDetails.get(pokemonName);
+
+                                    // Acceder a la información del Pokémon
+                                    String type = (String) pokemonDetails.get("type");
+                                    // int hp = ((Long) pokemonDetails.get("hp")).intValue();
+                                    // int attack = ((Long) pokemonDetails.get("attack")).intValue();
+                                    // int defense = ((Long) pokemonDetails.get("defense")).intValue();
+
+                                    // Puedes hacer más cosas con la información del Pokémon según tus necesidades
+                                    System.out.println("Pokemon: " + pokemonName);
+                                    System.out.println("Type: " + type);
+                                    // System.out.println("HP: " + hp);
+                                    // System.out.println("Attack: " + attack);
+                                    // System.out.println("Defense: " + defense);
+                                    System.out.println("------------------------");
+
+                                    // Incrementar la cantidad de Pokémon cargados
+                                    pokemonCarged++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return pokemonCarged;
+    }
 }
